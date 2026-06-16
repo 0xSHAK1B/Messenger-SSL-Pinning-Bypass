114cacb8bd1694bbe1654acccc7fc996fe674154
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-06-16 13:49:31 +0600

    Create traffic.sh

---
+++ traffic.sh
@@ -0,0 +1,22 @@
+POST /graphql HTTP/2
+host: b-graph.facebook.com
+x-fb-request-analytics-tags: {"network_tags":{"product":"256002347743983","request_category":"graphql","purpose":"none","retry_attempt":"0"},"application_tags":"graphservice"}
+x-fb-rmd: state=URL_ELIGIBLE
+priority: u=3, i
+user-agent: Dalvik/2.1.0 (Linux; U; Android 15; A001 Build/AP3A.240905.015.A2) [FBAN/Orca-Android;FBAV/565.0.0.43.88;FBPN/com.facebook.orca;FBLC/en_GB;FBBV/991058519;FBCR/null;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;FBDM/{density=2.625,width=1080,height=2392};FB_FW/1;]
+x-graphql-client-library: graphservice
+content-type: application/x-www-form-urlencoded
+x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
+content-encoding: gzip
+authorization: OAuth 256002347743983|374e60f8b9bb6b8cbb30f78030438895
+x-zero-state: unknown
+x-zero-f-device-id: c6c44de3-5dab-416f-98f4-25b534b14f83
+x-fb-friendly-name: FbBloksActionRootQuery-com.bloks.www.bloks.caa.login.async.send_login_request
+app-scope-id-header: c6c44de3-5dab-416f-98f4-25b534b14f83
+x-fb-connection-type: WIFI
+x-tigon-is-retry: False
+accept-encoding: gzip, deflate
+x-fb-http-engine: Tigon/Liger
+x-fb-client-ip: True
+x-fb-server-cluster: True
+x-fb-conn-uuid-client: vLWIsXSLt0I6fAirnULXXg==
