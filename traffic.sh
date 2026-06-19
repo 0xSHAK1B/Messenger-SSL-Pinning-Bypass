c1921f4f4fe44c6fe0f20896611856a886c900d6
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-06-19 18:06:39 +0600

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,9 +1,9 @@
-POST /graphql HTTP/2
+OST /graphql HTTP/2
 host: b-graph.facebook.com
 x-fb-request-analytics-tags: {"network_tags":{"product":"256002347743983","request_category":"graphql","purpose":"none","retry_attempt":"0"},"application_tags":"graphservice"}
 x-fb-rmd: state=URL_ELIGIBLE
 priority: u=3, i
-user-agent: Dalvik/2.1.0 (Linux; U; Android 15; A001 Build/AP3A.240905.015.A2) [FBAN/Orca-Android;FBAV/565.0.0.43.88;FBPN/com.facebook.orca;FBLC/en_GB;FBBV/991058519;FBCR/null;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;FBDM/{density=2.625,width=1080,height=2392};FB_FW/1;]
+user-agent: Dalvik/2.1.0 (Linux; U; Android 15; A001 Build/AP3A.240905.015.A2) [FBAN/Orca-Android;FBAV/566.0.0.57.86;FBPN/com.facebook.orca;FBLC/en_GB;FBBV/997326351;FBCR/null;FBMF/Nothing;FBBD/Nothing;FBDV/A001;FBSV/15;FBCA/arm64-v8a:null;FBDM/{density=2.625,width=1080,height=2392};FB_FW/1;]
 x-graphql-client-library: graphservice
 content-type: application/x-www-form-urlencoded
 x-zero-eh: 664c0faaac849cb891d0a261fbb72a12
@@ -19,4 +19,4 @@ accept-encoding: gzip, deflate
 x-fb-http-engine: Tigon/Liger
 x-fb-client-ip: True
 x-fb-server-cluster: True
-x-fb-conn-uuid-client: vLWIsXSLt0I6fAirnULXXg==
+x-fb-conn-uuid-client: lrt/PcuxWImlvqF3FhaM6w==
